788d02e690941311820ea1db4859db6001b73ed0
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-17 14:24:39 +0100

    Create Article Model

--- db/schema.rb
+++ db/schema.rb
@@ -18,5 +18,4 @@ ActiveRecord::Schema.define(version: 2022_02_17_125945) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end

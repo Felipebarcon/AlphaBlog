60181a5a23d0368a4799f517d1704abf229ecb6d
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-18 18:59:33 +0100

    links to other pages created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -37,6 +37,12 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
   private
 
   def articles_params

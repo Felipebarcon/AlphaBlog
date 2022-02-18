57994cf9cd348ec60a52f40faa8de6ffcead5a3b
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-18 18:28:02 +0100

    edit and update article ok

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,20 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(articles_params)
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+
   def create
     @article = Article.new(articles_params)
 

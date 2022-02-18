634ea77da907112dbf8cdfdecd430e52597eab49
Felipe Barcon <felipe.barcon@gmail.com>
2022-02-18 18:10:04 +0100

    new article setup and page controller created

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,4 +8,24 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+
+    if @article.save
+      flash[:notice] = "Article was created successfully !"
+      redirect_to @article
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :description)
+  end
 end

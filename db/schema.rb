65d52a2a59de19d0deaab7f7c87ecd42885135af
Felipe Barcon <felipe.barcon@gmail.com>
2022-04-18 18:35:36 +0200

    home page subtitle change

--- db/schema.rb
+++ db/schema.rb
@@ -18,4 +18,5 @@ ActiveRecord::Schema.define(version: 2022_02_17_125945) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end

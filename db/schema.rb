031b176237a02593f2552e35dd71d3e8fd8a8a35
Felipe Barcon <felipe.barcon@gmail.com>
2022-05-19 15:54:10 +0200

    add one to many between user and articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_19_120230) do
+ActiveRecord::Schema.define(version: 2022_05_19_134229) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description", limit: 16777215
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|

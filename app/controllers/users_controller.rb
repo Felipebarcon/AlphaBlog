cc8ef2173452ab5e13c4f5d5181c3ea8d8f141d6
Felipe Barcon <felipe.barcon@gmail.com>
2022-05-20 14:36:29 +0200

    creating index users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @articles = @user.articles
   end
 
+  def index
+    @users = User.all
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save

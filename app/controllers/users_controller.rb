7a07869ff4de1d140355e97ee5119fab0f449524
Felipe Barcon <felipe.barcon@gmail.com>
2022-05-20 10:47:23 +0200

    adding profile image and articles profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save

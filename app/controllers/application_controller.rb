75c6aade96f0cbedad275a94a2bd25a53306f7bd
Felipe Barcon <felipe.barcon@gmail.com>
2022-06-07 14:23:39 +0200

    Controller methods as helper mathods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :current_user, :logged_in?
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    # return true or false if a current user is logged in
+    !!current_user
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,13 +8,4 @@ module ApplicationHelper
     image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
   end
 
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    # return true or false if a current user is logged in
-    !!current_user
-  end
-
 end
